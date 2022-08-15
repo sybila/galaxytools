f127082d44c8f96baa7beb17689331a2396cea7a
Matej Trojak <trojak@mail.muni.cz>
2022-08-15 09:34:37 +0200

    Working example of PCTL model checking with docker dependency

---
+++ tools/ebcsgen/ebcsgen_pctl_model_checking.py
@@ -0,0 +1,40 @@
+import argparse
+
+from eBCSgen.Analysis.PCTL import PCTL
+from eBCSgen.Parsing.ParseBCSL import load_TS_from_json
+from eBCSgen.Errors.FormulaParsingError import FormulaParsingError
+from eBCSgen.Errors.InvalidInputError import InvalidInputError
+from eBCSgen.Parsing.ParsePCTLformula import PCTLparser
+
+
+args_parser = argparse.ArgumentParser(description='Model checking')
+
+args_parser._action_groups.pop()
+required = args_parser.add_argument_group('required arguments')
+optional = args_parser.add_argument_group('optional arguments')
+
+required.add_argument('--transition_file', required=True)
+required.add_argument('--output', type=str, required=True)
+required.add_argument('--formula', type=str, required=True)
+optional.add_argument('--local_storm', nargs="?", const=True)
+
+args = args_parser.parse_args()
+
+if args.local_storm:
+    local_storm = True
+else:
+    local_storm = False
+
+ts = load_TS_from_json(args.transition_file)
+
+if len(ts.params) != 0:
+    raise InvalidInputError("Provided transition system is parametrised - model checking cannot be executed.")
+
+formula = PCTLparser().parse(args.formula)
+if formula.success:
+    result = PCTL.model_checking(ts, formula, storm_local=True)
+    f = open(args.output, "w")
+    f.write(result.decode("utf-8"))
+    f.close()
+else:
+    raise FormulaParsingError(formula.data, args.formula)
